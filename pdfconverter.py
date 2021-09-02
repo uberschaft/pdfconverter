cbc70ee9fa5b5978e0ca9b176adef18bab96a769
uberschaft <54500213+uberschaft@users.noreply.github.com>
2021-09-02 22:58:18 +0300

    Update pdfconverter.py

--- pdfconverter.py
+++ pdfconverter.py
@@ -47,45 +47,29 @@ while (pdfcount < len(pdf_list)):
 
     def put_watermark(input_pdf, output_pdf, watermark):
 
-        # reads the watermark pdf file through
-        # PdfFileReader
         watermark_instance = PdfFileReader(watermark)
 
-        # fetches the respective page of
-        # watermark(1st page)
         watermark_page = watermark_instance.getPage(0)
 
-        # reads the input pdf file
         pdf_reader = PdfFileReader(pdfName)
 
-        # It creates a pdf writer object for the
-        # output file
         pdf_writer = PdfFileWriter()
 
-        # iterates through the original pdf to
-        # merge watermarks
         for page in range(pdf_reader.getNumPages()):
 
             page = pdf_reader.getPage(page)
 
-            # will overlay the watermark_page on top
-            # of the current page.
-            page.mergePage(watermark_page)
-
-            # add that newly merged page to the
-            # pdf_writer object.
             pdf_writer.addPage(page)
 
         with open(output_pdf, 'wb') as out:
 
-            # writes to the respective output_pdf provided
             pdf_writer.write(out)
 
     if __name__ == "__main__":
         put_watermark(
-            input_pdf=pdfName,  # the original pdf
-            output_pdf=workdir+pdfName,  # the modified pdf with watermark
-            watermark='watermark.pdf'  # the watermark to be provided
+            input_pdf=pdfName,
+            output_pdf=workdir+pdfName,
+            watermark='watermark.pdf'
         )
 
     print("**************watermark has been added**************\n**************Images are being converted**************")
